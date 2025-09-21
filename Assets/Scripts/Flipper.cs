f7de4d67bcfdb5a5482e6c08c293aaf78ba858bd
aidencuneo <aidencuneo@gmail.com>
2025-09-21 15:39:18 +1000

    Finished manual level layout

--- Assets/Scripts/Flipper.cs
+++ Assets/Scripts/Flipper.cs
@@ -8,6 +8,12 @@ public class Flipper : MonoBehaviour
 
     void Start()
     {
+        FlipHorizontal();
+        // FlipVertical();
+    }
+
+    void FlipHorizontal()
+    {
         for (int i = 0; i < transform.childCount; ++i)
         {
             Transform child = transform.GetChild(i);
@@ -20,6 +26,30 @@ public class Flipper : MonoBehaviour
                 180 => 90,
                 270 => 0,
                 0 => 270,
+                _ => child.rotation.eulerAngles.z,
+            };
+
+            if (child.name.Contains("Corner"))
+                n.transform.rotation = Quaternion.Euler(0, 0, newAngle);
+
+            n.transform.SetParent(otherTransform, true);
+        }
+    }
+
+    void FlipVertical()
+    {
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            Transform child = transform.GetChild(i);
+            GameObject n = Instantiate(child.gameObject);
+            n.transform.position = new(child.position.x, -child.position.y);
+
+            float newAngle = child.rotation.eulerAngles.z switch
+            {
+                0 => 90,
+                90 => 0,
+                180 => 270,
+                270 => 180,
                 _ => child.rotation.eulerAngles.z,
             };
 
