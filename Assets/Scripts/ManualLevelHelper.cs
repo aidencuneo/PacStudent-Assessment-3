d707742687a0194de7f8421d5a23dd48fa332dfe
aidencuneo <aidencuneo@gmail.com>
2025-09-21 13:25:52 +1000

    Made some more sprites and added helper script

---
+++ Assets/Scripts/ManualLevelHelper.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualLevelHelper : MonoBehaviour
+{
+    // This script acts as a helper for generating the manual level layout
+    // First, I run this script in game to place the prefabs (with default rotation), then I copy
+    // them back into the scene and rotate them correctly afterwards
+
+    public GameObject[] prefabs;
+
+    int[,] levelMap =
+    {
+        {1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 7},
+        {2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4},
+        {2, 5, 3, 4, 4, 3, 5, 3, 4, 4, 4, 3, 5, 4},
+        {2, 6, 4, 0, 0, 4, 5, 4, 0, 0, 0, 4, 5, 4},
+        {2, 5, 3, 4, 4, 3, 5, 3, 4, 4, 4, 3, 5, 3},
+        {2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
+        {2, 5, 3, 4, 4, 3, 5, 3, 3, 5, 3, 4, 4, 4},
+        {2, 5, 3, 4, 4, 3, 5, 4, 4, 5, 3, 4, 4, 3},
+        {2, 5, 5, 5, 5, 5, 5, 4, 4, 5, 5, 5, 5, 4},
+        {1, 2, 2, 2, 2, 1, 5, 4, 3, 4, 4, 3, 0, 4},
+        {0, 0, 0, 0, 0, 2, 5, 4, 3, 4, 4, 3, 0, 3},
+        {0, 0, 0, 0, 0, 2, 5, 4, 4, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 2, 5, 4, 4, 0, 3, 4, 4, 8},
+        {2, 2, 2, 2, 2, 1, 5, 3, 3, 0, 4, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 4, 0, 0, 0},
+    };
+
+    void Start()
+    {
+        for (int y = 0; y < levelMap.GetLength(0); y++)
+            for (int x = 0; x < levelMap.GetLength(1); x++)
+                if (prefabs[levelMap[y, x]])
+                    Instantiate(prefabs[levelMap[y, x]]).transform.position = new(x, y);
+    }
+}
