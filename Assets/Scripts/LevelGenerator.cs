ac44d1f58e864ad681089ea5a78cda66c87ecbd1
aidencuneo <aidencuneo@gmail.com>
2025-09-22 14:13:06 +1000

    Setting up level generator

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
     public bool shouldGenerateLevel = true;
+    public GameObject[] prefabs;
 
     // Level map to generate (top left corner)
     int[,] levelMap =
@@ -31,12 +32,53 @@ public class LevelGenerator : MonoBehaviour
         if (!shouldGenerateLevel)
             return;
 
+        // Clear the manual level layout
         ClearLevel();
+
+        // Generate the top left corner - to be used as a base for the rest of the level
+        GenerateBase();
+
+        // Mirror the base horizontally (children of this object will be used as the base)
+        MirrorBaseHorizontally();
+
+        // Mirror the top two segments vertically (the segments are the children of this object)
+        MirrorBaseVertically();
+    }
+
+    Transform Spawn(GameObject prefab, Vector2 pos)
+    {
+        Transform obj = Instantiate(prefab).transform;
+        obj.SetParent(transform);
+        obj.localPosition = pos; // Set local position, because this object defines the top left corner
+        return obj;
+    }
+
+    void SpawnAllFromMap(int[,] levelMap)
+    {
+        for (int y = 0; y < levelMap.GetLength(0); y++)
+            for (int x = 0; x < levelMap.GetLength(1); x++)
+                if (prefabs[levelMap[y, x]])
+                    Spawn(prefabs[levelMap[y, x]], new(x, -y));
     }
 
     void ClearLevel()
     {
         for (int i = 0; i < transform.childCount; ++i)
-            Destroy(transform.GetChild(i));
+            Destroy(transform.GetChild(i).gameObject);
+    }
+
+    void GenerateBase()
+    {
+        SpawnAllFromMap(levelMap);
+    }
+
+    void MirrorBaseHorizontally()
+    {
+
+    }
+
+    void MirrorBaseVertically()
+    {
+
     }
 }
