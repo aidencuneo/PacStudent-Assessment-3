cd994b075a17d3720fbd3a6d748a4f1726adafa1
aidencuneo <aidencuneo@gmail.com>
2025-10-29 11:40:20 +1100

    Bug fix

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -116,7 +116,7 @@ public class HUD : MonoBehaviour
 
     void Update()
     {
-        if (startTime != -1)
+        if (startTime != -1 && lives > 0)
             gameTime = Time.unscaledTime - startTime;
     }
 
