f122397d8533cc519dd22393a0ef795b55e56f9a
aidencuneo <aidencuneo@gmail.com>
2025-10-29 21:29:14 +1100

    Finished immersive camera

--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -4,18 +4,8 @@ using UnityEngine;
 
 public class SmoothFollow : MonoBehaviour
 {
-    public static SmoothFollow me;
     public Transform target;
-
-    Rigidbody2D rb;
-    public Camera cam;
-
-    void Awake()
-    {
-        me = this;
-        cam = GetComponent<Camera>();
-        rb = target.GetComponent<Rigidbody2D>();
-    }
+    public float speed = 0.05f;
 
     void LateUpdate()
     {
@@ -25,6 +15,6 @@ public class SmoothFollow : MonoBehaviour
         Vector3 targetPos = target.position;
         targetPos.z = -10;
 
-        transform.position = 0.8f * transform.position + 0.2f * targetPos;
+        transform.position = (1 - speed) * transform.position + speed * targetPos;
     }
 }
